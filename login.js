ccb66b608d68ff267fdfae098aaa6708dda13443
njells <angelicalidya05@gmail.com>
2024-06-03 01:22:27 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+const loginBtn = document.getElementById('login');
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
